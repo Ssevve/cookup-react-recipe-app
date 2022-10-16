59fa48ad4ab98a491edbafafed2fc5ad20e00054
Seweryn <ssevve@gmail.com>
2022-10-16 15:41:03 +0200

    Add password encryption and decryption to user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const { Schema } = mongoose;
 
@@ -11,15 +12,50 @@ const userSchema = new Schema(
   {
     firstName: requiredString,
     lastName: requiredString,
-    email: requiredString,
+    email: { ...requiredString, unique: true },
     password: requiredString,
     avatar: {
       type: String,
-      default: 'https://res.cloudinary.com/dj50j2x97/image/upload/v1665922005/150_whwxod.jpg',
+      default:
+        'https://res.cloudinary.com/dj50j2x97/image/upload/v1665922005/150_whwxod.jpg',
     },
   },
   { timestamps: true },
 );
+
+// Password hash middleware.
+
+// eslint-disable-next-line consistent-return
+userSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    // eslint-disable-next-line no-shadow
+    return bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+// Helper method for validating user's password.
+
+userSchema.methods.comparePassword = function comparePassword(
+  candidatePassword,
+  cb,
+) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
