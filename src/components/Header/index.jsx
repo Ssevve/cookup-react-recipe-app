b0479eceff2df3810ad37a1c0b7cff5b9654b09c
Seweryn <ssevve@gmail.com>
2022-10-17 19:26:10 +0200

    Add logout functionality

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,27 @@ import { NavLink } from 'react-router-dom';
 
 import './style.css';
 
-export default function Header() {
+export default function Header({ user, setUser }) {
+  async function logout(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:8000/api/auth/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+
+      const data = await res.json();
+
+      if (res.status === 200) {
+        setUser(null);
+        console.log(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <header className="header">
       <div className="container flex justify-content-sb align-items-center">
@@ -12,28 +32,59 @@ export default function Header() {
         <nav className="header__nav">
           <ul className="header__link-list flex">
             <li>
-              <NavLink activeclassname='active' className="btn header__link" to="/" end>
+              <NavLink
+                activeclassname="active"
+                className="btn header__link"
+                to="/"
+                end
+              >
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink activeclassname='active' className="btn header__link" to="/recipes">
+              <NavLink
+                activeclassname="active"
+                className="btn header__link"
+                to="/recipes"
+              >
                 Recipes
               </NavLink>
             </li>
             <li>
-              <NavLink activeclassname='active' className="btn header__link" to="/add">
+              <NavLink
+                activeclassname="active"
+                className="btn header__link"
+                to="/add"
+              >
                 Add Recipe
               </NavLink>
             </li>
             <li>
-              <NavLink activeclassname='active' className="btn header__link" to="/login">
+              <NavLink
+                activeclassname="active"
+                className="btn header__link"
+                to="/login"
+              >
                 Login
               </NavLink>
             </li>
             <li>
-              <NavLink activeclassname='active' className="btn header__link" to="/signup">
+              <NavLink
+                activeclassname="active"
+                className="btn header__link"
+                to="/signup"
+              >
                 Signup
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                onClick={logout}
+                activeclassname="active"
+                className="btn header__link"
+                to="#"
+              >
+                Logout
               </NavLink>
             </li>
           </ul>
