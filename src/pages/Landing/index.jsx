35b4d9cb77d53bb4b450c6131a8c3e225c1d68a1
Seweryn <ssevve@gmail.com>
2022-11-06 02:10:50 +0100

    Rewrite mobile navbar and landing styles to use css modules and grid

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -9,25 +9,27 @@ import Button from '../../components/Button';
 export default function Landing() {
   return (
     <main className={styles.hero}>
-      <Container>
-        <h1 className={styles.heroTitle}>
-          Cookup.
-          <span className={styles.highlightedText}>Whatever</span>
-          you want.
-        </h1>
-        <p className={styles.heroCopy}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore asperiores adipisci minima
-          quo nulla sequi quas eos deleniti consectetur dolor?
-        </p>
-        <div className={styles.buttons}>
-          <Link to="/signup">
-            <Button text="Signup" className={styles.btn} />
-          </Link>
-          <Link to="/signup">
-            <Button text="Browse Recipes" variant="outline" className={styles.btn} />
-          </Link>
-        </div>
-        <section className="hero-image" />
+      <Container className={styles.container}>
+        <section className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>
+            Cookup.
+            <span className={styles.highlightedText}>Whatever</span>
+            you want.
+          </h1>
+          <p className={styles.heroCopy}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore asperiores adipisci
+            minima quo nulla sequi quas eos deleniti consectetur dolor?
+          </p>
+          <div className={styles.buttons}>
+            <Link to="/signup">
+              <Button text="Signup" className={styles.btn} />
+            </Link>
+            <Link to="/recipes">
+              <Button text="Browse Recipes" variant="outline" className={styles.btn} />
+            </Link>
+          </div>
+        </section>
+        <div className={styles.heroImage} />
       </Container>
     </main>
   );
