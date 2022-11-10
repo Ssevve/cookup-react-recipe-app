c4e2acc0f2d8be9e474c7999243b93115a2b7f4a
Seweryn <ssevve@gmail.com>
2022-11-10 11:15:27 +0100

    Start landing page styling

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import cx from 'classnames';
 
 import styles from './landing.module.css';
 
 export default function Landing() {
   return (
-    <main className={styles.hero}>
-      <div className={styles.container}>
-        <section className={styles.heroContent}>
-          <h1 className={styles.heroTitle}>
-            Cookup.
-            <span className={styles.highlightedText}>Whatever</span>
-            you want.
-          </h1>
-          <p className={styles.heroCopy}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore asperiores adipisci
-            minima quo nulla sequi quas eos deleniti consectetur dolor?
-          </p>
-          <div className={styles.buttons}>
-            <Link className={styles.btn} to="/signup">
-              Signup
-            </Link>
-            <Link className={styles.btnOutline} to="/recipes">
-              Browse recipes
-            </Link>
-          </div>
-        </section>
-        <div className={styles.heroImage} />
-      </div>
-    </main>
+    <div className={styles.container}>
+      <section className={styles.content}>
+        <h1 className={styles.title}>
+          Cookup.
+          <span className={styles.highlightedText}>Whatever</span>
+          you want.
+        </h1>
+        <p className={styles.copy}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore asperiores adipisci minima
+          quo nulla sequi quas eos deleniti consectetur dolor?
+        </p>
+        <div className={styles.buttons}>
+          <Link className={cx(styles.btn, styles.cta)} to="/signup">
+            Signup
+          </Link>
+          <Link className={cx(styles.btn, styles.outline)} to="/recipes">
+            Browse recipes
+          </Link>
+        </div>
+      </section>
+      <div className={styles.heroImage} />
+    </div>
   );
 }
