7ad9a67e8be5bb195611b87e1914f4814c4b862c
Seweryn <ssevve@gmail.com>
2022-11-13 19:41:53 +0100

    Make working signup requests

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import cx from 'classnames';
 import { useForm } from 'react-hook-form';
 
 import styles from './signup.module.css';
 
 export default function Signup() {
+  const [responseErrors, setResponseErrors] = useState({});
   const navigate = useNavigate();
   const {
     register,
@@ -17,45 +19,44 @@ export default function Signup() {
   async function handleFormSubmit(data) {
     console.log(data);
 
-    // const url = 'http://localhost:8000/api/auth/signup';
-    // const requestOptions = {
-    //   method: 'POST',
-    //   headers: {
-    //     'content-type': 'application/json',
-    //   },
-    //   body: JSON.stringify(userInput),
-    // };
+    const url = 'http://localhost:8000/api/auth/signup';
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
 
-    // try {
-    //   const res = await fetch(url, requestOptions);
-    //   const data = await res.json();
+    try {
+      const res = await fetch(url, requestOptions);
+      const resData = await res.json();
 
-    //   if (!res.ok) {
-    //     setFormErrors([data.message]);
-    //   } else {
-    //     navigate('/login');
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
+      if (res.status === 400 || res.status === 409) setResponseErrors(resData.message);
+      else if (res.status === 404) setResponseErrors({ notFound: 'Something went wrong.' });
+      else if (res.status === 500) setResponseErrors({ serverError: 'Server error.' });
+      else navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className={styles.container}>
       <section className={styles.formSection}>
-        <h1 className={styles.title}>Signup</h1>
+        <h1 className={styles.title}>Sign up</h1>
         <p className={styles.existingUser}>
           Existing user?
           <Link className={styles.link} to="/login">
             Log in
           </Link>
         </p>
-        {/* {(errors.email || errors.password) && (
+        {(responseErrors.notFound || responseErrors.serverError) && (
           <div className={styles.errorBox}>
-            <span role="alert">Incorrect email or password.</span>
+            <span role="alert">{responseErrors.notFound || responseErrors.serverError}</span>
             <span role="alert">Please try again.</span>
           </div>
-        )} */}
+        )}
         <form className={styles.form} onSubmit={handleSubmit(handleFormSubmit)} noValidate>
           <div className="form-group">
             <label className={styles.label} htmlFor="first-name">
@@ -64,13 +65,18 @@ export default function Signup() {
                 {...register('firstName', {
                   required: { value: true, message: 'First name is required' },
                 })}
-                className={styles.input}
+                className={cx(
+                  styles.input,
+                  errors.firstName && styles.error,
+                )}
                 id="first-name"
                 type="text"
                 name="firstName"
               />
             </label>
-            <span role="alert" className={styles.errorMessage}>{errors?.firstName?.message}</span>
+            <span role="alert" className={styles.errorMessage}>
+              {errors?.firstName?.message}
+            </span>
           </div>
           <div className="form-group">
             <label className={styles.label} htmlFor="last-name">
@@ -79,13 +85,18 @@ export default function Signup() {
                 {...register('lastName', {
                   required: { value: true, message: 'Last name is required' },
                 })}
-                className={styles.input}
+                className={cx(
+                  styles.input,
+                  errors.lastName && styles.error,
+                )}
                 id="last-name"
                 type="text"
                 name="lastName"
               />
             </label>
-            <span role="alert" className={styles.errorMessage}>{errors?.lastName?.message}</span>
+            <span role="alert" className={styles.errorMessage}>
+              {errors?.lastName?.message}
+            </span>
           </div>
           <div className="form-group">
             <label className={styles.label} htmlFor="email">
@@ -99,13 +110,18 @@ export default function Signup() {
                     message: 'Incorrect email format',
                   },
                 })}
-                className={styles.input}
+                className={cx(
+                  styles.input,
+                  (errors.email || responseErrors.email) && styles.error,
+                )}
                 id="email"
                 type="email"
                 name="email"
               />
             </label>
-            <span role="alert" className={styles.errorMessage}>{errors?.email?.message}</span>
+            <span role="alert" className={styles.errorMessage}>
+              {errors?.email?.message || responseErrors?.email}
+            </span>
           </div>
           <div className="form-group">
             <label className={styles.label} htmlFor="password">
@@ -115,16 +131,21 @@ export default function Signup() {
                   required: { value: true, message: 'Password is required' },
                   minLength: {
                     value: 8,
-                    message: 'Password needs to be at least 8 characters long',
+                    message: 'Min. 8 characters required',
                   },
                 })}
-                className={styles.input}
+                className={cx(
+                  styles.input,
+                  errors.password && styles.error,
+                )}
                 id="password"
                 type="password"
                 name="password"
               />
             </label>
-            <span role="alert" className={styles.errorMessage}>{errors?.password?.message}</span>
+            <span role="alert" className={styles.errorMessage}>
+              {errors?.password?.message}
+            </span>
           </div>
           <div className="form-group">
             <label className={styles.label} htmlFor="confirm-password">
@@ -139,13 +160,18 @@ export default function Signup() {
                     }
                   },
                 })}
-                className={styles.input}
+                className={cx(
+                  styles.input,
+                  errors.confirmPassword && styles.error,
+                )}
                 id="confirm-password"
                 type="password"
                 name="confirmPassword"
               />
             </label>
-            <span role="alert" className={styles.errorMessage}>{errors?.confirmPassword?.message}</span>
+            <span role="alert" className={styles.errorMessage}>
+              {errors?.confirmPassword?.message}
+            </span>
           </div>
           <button className={styles.btn} type="submit">
             Signup
