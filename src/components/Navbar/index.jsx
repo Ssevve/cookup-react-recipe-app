94b927caa625e284713961abf23f33fa7879d5b0
Seweryn <ssevve@gmail.com>
2022-11-19 02:34:25 +0100

    Add profile dropdown with profile link and logout option

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,6 +9,7 @@ import styles from './navbar.module.css';
 
 export default function Navbar({ user, setUser }) {
   const [showMenu, setShowMenu] = useState(false);
+  const [showProfileDropdown, setShowProfileDropdown] = useState(false);
   const navigate = useNavigate();
   const logout = async (e) => {
     e.preventDefault();
@@ -23,6 +24,7 @@ export default function Navbar({ user, setUser }) {
         setUser(null);
         navigate('/');
         setShowMenu(false);
+        setShowProfileDropdown(false);
       }
     } catch (err) {
       // eslint-disable-next-line no-console
@@ -52,7 +54,8 @@ export default function Navbar({ user, setUser }) {
             <GoThreeBars />
           </button>
         </div>
-        <ul className={cx(styles.navMenu, showMenu && styles.expanded)}>
+        {/* MOBILE NAV */}
+        <ul className={cx(styles.navMenu, styles.mobileMenu, showMenu && styles.expanded)}>
           {!user && (
             <>
               <li className={styles.navItem}>
@@ -84,11 +87,7 @@ export default function Navbar({ user, setUser }) {
                 </NavLink>
               </li>
               <li className={styles.navItem}>
-                <Link
-                  onClick={() => setShowMenu(false)}
-                  className={styles.btn}
-                  to="/signup"
-                >
+                <Link onClick={() => setShowMenu(false)} className={styles.btn} to="/signup">
                   Sign up
                 </Link>
               </li>
@@ -124,13 +123,95 @@ export default function Navbar({ user, setUser }) {
                 </NavLink>
               </li>
               <li className={styles.navItem}>
-                <button
-                  type="button"
-                  onClick={logout}
-                  className={styles.navLink}
-                >
+                <button type="button" onClick={logout} className={styles.navLink}>
                   Log out
                 </button>
+              </li>
+            </>
+          )}
+        </ul>
+        {/* DESKTOP NAV */}
+        <ul className={cx(styles.navMenu, styles.desktopMenu, showMenu && styles.expanded)}>
+          {!user && (
+            <>
+              <li className={styles.navItem}>
+                <NavLink
+                  onClick={() => setShowMenu(false)}
+                  className={({ isActive }) => (isActive ? cx(styles.navLink, styles.active) : styles.navLink)}
+                  to="/"
+                  end
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <NavLink
+                  onClick={() => setShowMenu(false)}
+                  className={({ isActive }) => (isActive ? cx(styles.navLink, styles.active) : styles.navLink)}
+                  to="/recipes/browse"
+                >
+                  Browse Recipes
+                </NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <NavLink
+                  onClick={() => setShowMenu(false)}
+                  className={({ isActive }) => (isActive ? cx(styles.navLink, styles.active) : styles.navLink)}
+                  to="/login"
+                >
+                  Log in
+                </NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <Link onClick={() => setShowMenu(false)} className={styles.btn} to="/signup">
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
+          {user && (
+            <>
+              <li className={styles.navItem}>
+                <NavLink
+                  onClick={() => setShowMenu(false)}
+                  className={({ isActive }) => (isActive ? cx(styles.navLink, styles.active) : styles.navLink)}
+                  to="/recipes/browse"
+                >
+                  Browse recipes
+                </NavLink>
+              </li>
+              <li className={styles.navItem}>
+                <NavLink
+                  onClick={() => setShowMenu(false)}
+                  className={({ isActive }) => (isActive ? cx(styles.navLink, styles.active) : styles.navLink)}
+                  to="/recipes/add"
+                >
+                  Add recipe
+                </NavLink>
+              </li>
+              <li onMouseLeave={() => setShowProfileDropdown(false)} className={cx(styles.navItem, styles.profileDropdown)}>
+                <button className={styles.profileDropdownButton} onClick={() => setShowProfileDropdown((prev) => !prev)} type="button">
+                  <img className={styles.avatar} src={user.avatar.url} alt={`${user.firstName} ${user.lastName}`} />
+                  <span className={styles.userName}>{`${user.firstName} ${user.lastName}`}</span>
+                </button>
+                {showProfileDropdown && (
+                  <ul className={styles.profileDropdownMenu}>
+                    <li>
+                      <Link
+                        onClick={() => setShowProfileDropdown(false)}
+                        className={styles.dropdownLink}
+                        to="/dashboard"
+                      >
+                        Profile
+                      </Link>
+                    </li>
+                    <li>
+                      <button type="button" onClick={logout} className={styles.dropdownLink}>
+                        Log out
+                      </button>
+                    </li>
+                  </ul>
+                )}
               </li>
             </>
           )}
