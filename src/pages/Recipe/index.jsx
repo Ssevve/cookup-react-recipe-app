342da109241ff5ee23695a84c8402fe5ed3cabbd
Seweryn <ssevve@gmail.com>
2022-11-22 04:03:23 +0100

    Add ingredients and directions with mobile styling

--- src/pages/Recipe/index.jsx
+++ src/pages/Recipe/index.jsx
@@ -45,53 +45,77 @@ export default function Recipe() {
             {recipe.likes.length}
           </span>
         </div>
-        <div className={styles.actionButtons}>
-          {loggedInUser && recipe.createdBy !== loggedInUser.id && (
-            <button type="button" className={cx(styles.actionBtn, styles.likeBtn)}>
-              {loggedInUser && recipe?.likes?.includes(loggedInUser.id) ? (
-                <>
-                  <span>Liked</span>
-                  <AiFillHeart size={20} />
-                </>
-              ) : (
-                <>
-                  <span>Like</span>
-                  <AiOutlineHeart size={20} />
-                </>
-              )}
-            </button>
-          )}
-          {loggedInUser && recipe.createdBy === loggedInUser.id && (
-            <>
-              <Link
-                to={`/recipes/edit/${recipe._id}`}
-                className={cx(styles.actionBtn, styles.editBtn)}
-              >
-                Edit
-              </Link>
-              <button type="button" className={cx(styles.actionBtn, styles.deleteBtn)}>
-                Delete
+        {loggedInUser && (
+          <div className={styles.actionButtons}>
+            {recipe.createdBy !== loggedInUser.id && (
+              <button type="button" className={cx(styles.actionBtn, styles.likeBtn)}>
+                {recipe.likes.includes(loggedInUser.id) ? (
+                  <>
+                    <span>Liked</span>
+                    <AiFillHeart size={20} />
+                  </>
+                ) : (
+                  <>
+                    <span>Like</span>
+                    <AiOutlineHeart size={20} />
+                  </>
+                )}
               </button>
-            </>
-          )}
-        </div>
+            )}
+            {recipe.createdBy === loggedInUser.id && (
+              <>
+                <Link
+                  to={`/recipes/edit/${recipe._id}`}
+                  className={cx(styles.actionBtn, styles.editBtn)}
+                >
+                  Edit
+                </Link>
+                <button type="button" className={cx(styles.actionBtn, styles.deleteBtn)}>
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+        )}
         <ImageCarousel images={recipe.images} recipeName={recipe.name} />
-        <section className={styles.details}>
-          <div>
-            <span className={styles.detailsLabel}>Prep time:</span>
-            <span className={styles.detail}>{`${recipe.prepTime.time} ${recipe.prepTime.unit}`}</span>
+        <section className={styles.detailSection}>
+          <h2 className={styles.sectionHeading}>Details</h2>
+          <div className={styles.details}>
+            <div>
+              <span className={styles.detailsLabel}>Prep time:</span>
+              <span className={styles.detail}>{`${recipe.prepTime.time} ${recipe.prepTime.unit}`}</span>
+            </div>
+            <div>
+              <span className={styles.detailsLabel}>Cook time:</span>
+              <span className={styles.detail}>{`${recipe.cookTime.time} ${recipe.cookTime.unit}`}</span>
+            </div>
+            <div>
+              <span className={styles.detailsLabel}>Servings:</span>
+              <span className={styles.detail}>{recipe.servings}</span>
+            </div>
+            <div>
+              <span className={styles.detailsLabel}>Difficulty:</span>
+              <span className={styles.detail}>{recipe.difficulty}</span>
+            </div>
           </div>
-          <div>
-            <span className={styles.detailsLabel}>Cook time:</span>
-            <span className={styles.detail}>{`${recipe.cookTime.time} ${recipe.cookTime.unit}`}</span>
-          </div>
-          <div>
-            <span className={styles.detailsLabel}>Servings:</span>
-            <span className={styles.detail}>{recipe.servings}</span>
-          </div>
-          <div>
-            <span className={styles.detailsLabel}>Difficulty:</span>
-            <span className={styles.detail}>{recipe.difficulty}</span>
+        </section>
+        <section className={styles.section}>
+          <h2 className={styles.sectionHeading}>Ingredients</h2>
+          <ul className={styles.ingredients}>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient} className={styles.ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <h2 className={styles.sectionHeading}>Directions</h2>
+          <div className={styles.directions}>
+            {recipe.directions.map((direction, index) => (
+              <div key={direction}>
+                <h3 className={styles.step}>{`Step ${index + 1}`}</h3>
+                <p className={styles.direction}>{direction}</p>
+              </div>
+            ))}
           </div>
         </section>
       </div>
