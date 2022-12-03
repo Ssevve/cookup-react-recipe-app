c159012cd8bb6be543918da0f3e33091734487e1
Seweryn <ssevve@gmail.com>
2022-12-03 19:19:01 +0100

    Add prop types to RecipeCard component

--- src/components/RecipeCard/index.jsx
+++ src/components/RecipeCard/index.jsx
@@ -1,9 +1,6 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable max-len */
-// /* eslint-disable no-underscore-dangle */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { AiOutlineHeart } from 'react-icons/ai';
 
@@ -23,7 +20,14 @@ export default function RecipeCard({
             src={recipe.images[0]?.url || '/images/placeholder-recipe-image.jpg'}
             alt={recipe.name}
           />
-          <LikeButton className={styles.likeBtn} setRecipes={setRecipes} recipes={recipes} recipe={recipe} user={user} round />
+          <LikeButton
+            className={styles.likeBtn}
+            setRecipes={setRecipes}
+            recipes={recipes}
+            recipe={recipe}
+            user={user}
+            round
+          />
           <span className={styles.likes}>
             <AiOutlineHeart />
             {recipe.likes.length}
@@ -37,3 +41,22 @@ export default function RecipeCard({
     </Link>
   );
 }
+
+RecipeCard.propTypes = {
+  setRecipes: PropTypes.func.isRequired,
+  recipes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  recipe: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    likes: PropTypes.arrayOf(PropTypes.string).isRequired,
+    dishType: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.shape({
+      url: PropTypes.string.isRequired,
+    })).isRequired,
+  }).isRequired,
+  user: PropTypes.shape({}),
+};
+
+RecipeCard.defaultProps = {
+  user: null,
+};
