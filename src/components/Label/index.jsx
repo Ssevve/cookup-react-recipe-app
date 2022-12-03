caf95e1cda4cb74f14da9402035d8c981ee147fb
Seweryn <ssevve@gmail.com>
2022-12-03 18:44:56 +0100

    Add prop types to Label component

--- src/components/Label/index.jsx
+++ src/components/Label/index.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './label.module.css';
 
-export default function Form({
+export default function Label({
   children, htmlFor, text, ...rest
 }) {
   return (
@@ -14,3 +14,12 @@ export default function Form({
     </label>
   );
 }
+
+Label.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  htmlFor: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
