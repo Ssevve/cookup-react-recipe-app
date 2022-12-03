bd4de12525355ec60b235535510995abc7662ec1
Seweryn <ssevve@gmail.com>
2022-12-03 19:09:33 +0100

    Add prop types to PageTitle component

--- src/components/PageTitle/index.jsx
+++ src/components/PageTitle/index.jsx
@@ -1,13 +1,25 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import styles from './pageTitle.module.css';
 
-export default function AddRecipe({ children, big }) {
+export default function PageTitle({ children, big }) {
   return (
     <h1 className={cx(styles.title, big && styles.big)}>
       {children}
     </h1>
   );
 }
+
+PageTitle.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  big: PropTypes.bool,
+};
+
+PageTitle.defaultProps = {
+  big: false,
+};
